c280f3fb79008c2a6fd69dedd2bb25e64681d54a
Henry Le <henryle@MacBook-Air.local>
2026-01-21 15:55:59 -0500

    fixed NavBar.css impor capitalization

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import { GoogleLogin } from "@react-oauth/google";
 import { Link } from "react-router-dom";
 import { UserContext } from "./App";
 import { useContext } from "react";
-import "./Navbar.css";
+import "./NavBar.css";
 
 const NavBar = () => {
   const { userId, handleLogin, handleLogout } = useContext(UserContext);
